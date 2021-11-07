60673d67b0f1cc022ffb1fa076844f14309f03ca
Edward Zaplotynskyi <edward.downtown@gmail.com>
2021-11-07 23:11:10 +0200

    first T done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const docBody = document.querySelector('body');
+let timer = null;
+
+startButton.addEventListener('click', handleStart);
+stopButton.addEventListener('click', handleStop);
+
+function handleStart() {
+    docBody.style.backgroundColor = getRandomHexColor();
+    startButton.disabled = true;
+    timer = setInterval(() => {
+        docBody.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+
+function handleStop() {
+    clearInterval(timer);
+    startButton.disabled = false;
+}
 
