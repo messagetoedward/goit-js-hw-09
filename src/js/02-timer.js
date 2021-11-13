f58616a98f6be734d63637a7d4fae2ba36f0a1d2
Edward Zaplotynskyi <edward.downtown@gmail.com>
2021-11-13 20:01:54 +0200

    3 rady

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,9 +1,14 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix, { Notify } from 'notiflix';
 
 const startButton = document.querySelector('button[data-start]')
+const dateInput = document.querySelector('input')
 const picker = document.querySelector('#datetime-picker');
-const now = new Date();
+const timerDays = document.querySelector('[data-days]');
+const timerHours = document.querySelector('[data-hours]');
+const timerMinutes = document.querySelector('[data-minutes]');
+const timerSeconds = document.querySelector('[data-seconds]')
 
 startButton.disabled = true;
 
@@ -11,29 +16,53 @@ const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
-    minuteIncrement: 1,
+  minuteIncrement: 1,
   onClose(selectedDates) {
       console.log(selectedDates[0]);
       if (selectedDates[0] <= options.defaultDate) {
           startButton.disabled = true;
-          window.alert("Please choose a date in the future")
+          return Notify.failure("Please choose a date in the future");
 
       } else {
           startButton.disabled = false;
-          console.log(selectedDates[0].getTime());
-          console.log(this.defaultDate);
+          return parseDate(selectedDates);
       }
   },
 };
 
 let date = flatpickr(picker, options);
-
-// console.log(date.selectedDates[0].getTime() - now.getTime());
-
-// const pickedDate = date.selectedDates.getTime();
+let selectedInMs = null;
 
 
-// convertMs(date.selectedDates[0].getTime())
+startButton.addEventListener('click', (() => {
+    dateInput.disabled = true;
+    const timer = setInterval(() => {
+        const deltaTime = selectedInMs - Date.now();
+        const remainingMs = convertMs(deltaTime);
+        if (selectedInMs - Date.now() > 0) {
+            timeMarkup(remainingMs);
+
+        } else {
+            clearInterval(timer)
+            Notify.success('Time is up!')
+            startButton.disabled = false;
+            dateInput.disabled = false;
+        }
+        
+    }, 1000);
+    startButton.disabled = true;
+}));
+
+function parseDate(date) {
+    return selectedInMs = Date.parse(date)
+}
+// let remainingMs = convertMs(clickMs);
+function timeMarkup({ days, hours, minutes, seconds }) {
+    timerDays.textContent = `${days}`;
+    timerHours.textContent = `${hours}`;
+    timerMinutes.textContent = `${minutes}`;
+    timerSeconds.textContent = `${seconds}`;
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -43,13 +72,16 @@ function convertMs(ms) {
   const day = hour * 24;
 
   // Remaining days
-  const days = Math.floor(ms / day);
+  const days = addLeadingZero(Math.floor(ms / day));
   // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
   // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
   // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
   return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
 }
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,40 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+import Notiflix, { Notify } from 'notiflix';
+const form = document.querySelector('.form');
+const inputDelay = document.querySelector('input[name="delay"]');
+const stepDelay = document.querySelector('input[name="step"]');
+const amount = document.querySelector('input[name="amount"]');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  let firstDelay = Number(inputDelay.value);
+  let delayStep = Number(stepDelay.value);
+  let amountNumber = Number(amount.value);
+
+console.log(firstDelay, delayStep, amountNumber);
+
+  for (let position = 1; position <= amountNumber; position++) {
+    console.log(position, firstDelay);
+    createPromise(position, firstDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    firstDelay += delayStep;
   }
-}
+});
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay })
+      } else {
+        reject({ position, delay })
+      }
+    }, delay);
+  });
+};
